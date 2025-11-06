2e2d175f563ca9c577f2273f708f2f78e556bc5e
anggimoo <H098S02919@chunchon.gwe.hs.kr>
2025-11-06 09:23:20 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,9 +75,9 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
 
-labels[0]:{"texts":[나는 볶음밥을 내 여친이랑 먹고 싶어]},
-labels[1]:{"texts":[나는 짜장면을 내 여친이랑 먹고 싶어]},
-labels[2]:{"texts":[나는 짬뽕을 내 여친이랑 먹고 싶어]},
+labels[0]:{"texts":["나는 볶음밥을 내 여친이랑 먹고 싶어"]},
+labels[1]:{"texts":["나는 짜장면을 내 여친이랑 먹고 싶어"]},
+labels[2]:{"texts":["나는 짬뽕을 내 여친이랑 먹고 싶어"]},
 
 
 # ======================
